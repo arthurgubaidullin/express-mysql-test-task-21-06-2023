b7466a4714bfd84a776469c42e0c63f6af7f714a
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-21 22:53:47 +0400

    Add handler

---
+++ apps/rest/src/file/download-file/handler.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import * as Option from 'fp-ts/Option';
+import { FileRepository } from '../repository/instance';
+import { TypeWithFileId } from '../type-with-file-id';
+import { isLeft } from 'fp-ts/Either';
+
+export async function downloadFileHandler(
+  req: express.Request<unknown>,
+  res: express.Response<never>
+): Promise<void> {
+  const params = TypeWithFileId.decode(req.params);
+
+  if (isLeft(params)) {
+    res.status(400).end();
+    return;
+  }
+
+  const file = FileRepository.getFileRecord(params.right.fileId);
+
+  if (Option.isNone(file)) {
+    res.status(400).end();
+    return;
+  }
+
+  res.setHeader('content-type', file.value.mimeType);
+  res.sendFile(file.value.path);
+}
