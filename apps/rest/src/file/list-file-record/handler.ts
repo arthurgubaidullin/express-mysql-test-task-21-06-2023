138dbfbf787f03b30b5edae2b2bd7f67b0943fd6
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-21 22:50:11 +0400

    Add handler

---
+++ apps/rest/src/file/list-file-record/handler.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import * as t from 'io-ts';
+import { isLeft } from 'fp-ts/Either';
+import { listFileRecord } from './use-case';
+import { ListRequest } from './request';
+import { ListResponse } from './response';
+
+export function listFileRecordHandler(
+  req: express.Request<unknown>,
+  res: express.Response<t.OutputOf<typeof ListResponse>>
+): void {
+  const query = ListRequest.decode(req.query);
+
+  if (isLeft(query)) {
+    res.status(400).end();
+    return;
+  }
+
+  const result = listFileRecord({
+    ...query.right,
+    listSize: query.right.list_size,
+  });
+
+  res
+    .json(
+      ListResponse.encode({
+        list: result.list,
+        list_size: result.listSize,
+        page: result.page,
+      })
+    )
+    .end();
+}
