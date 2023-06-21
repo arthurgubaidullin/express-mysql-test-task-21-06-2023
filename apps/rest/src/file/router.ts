3c0e8c97dd2ba335b97a6f3709e0f131f2382abe
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-21 22:52:45 +0400

    Enable handler

--- apps/rest/src/file/router.ts
+++ apps/rest/src/file/router.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import path from 'path';
 import { cwd } from 'process';
 import { uploadFileHandler } from './upload-file/handler';
+import { getFileRecordHandler } from './get-file-record/handler';
 import { listFileRecordHandler } from './list-file-record/handler';
 
 const UPLOADS_PATH = path.join(cwd(), 'uploads');
@@ -14,5 +15,7 @@ const file = express.Router();
 file.post('/upload', upload.single('file'), uploadFileHandler);
 
 file.get('/list', listFileRecordHandler);
+
+file.get('/:fileId', getFileRecordHandler);
 
 export default file;
