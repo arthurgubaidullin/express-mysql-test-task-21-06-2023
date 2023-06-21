8cf88ca471e058743af6bb8320a75c01f2b8058e
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-21 22:54:05 +0400

    Enable handler

--- apps/rest/src/file/router.ts
+++ apps/rest/src/file/router.ts
@@ -6,6 +6,7 @@ import { uploadFileHandler } from './upload-file/handler';
 import { getFileRecordHandler } from './get-file-record/handler';
 import { listFileRecordHandler } from './list-file-record/handler';
 import { deleteFileRecordHandler } from './delete-file-record/handler';
+import { downloadFileHandler } from './download-file/handler';
 
 const UPLOADS_PATH = path.join(cwd(), 'uploads');
 
@@ -18,6 +19,8 @@ file.post('/upload', upload.single('file'), uploadFileHandler);
 file.get('/list', listFileRecordHandler);
 
 file.delete('/delete/:fileId', deleteFileRecordHandler);
+
+file.get('/download/:fileId', downloadFileHandler);
 
 file.get('/:fileId', getFileRecordHandler);
 
