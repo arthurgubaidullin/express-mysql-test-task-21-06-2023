8567003ab089a677340150aa618f81f5e03ea42e
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-22 12:26:04 +0400

    Add method to repo

--- apps/rest/src/auth/repository/in-memory.ts
+++ apps/rest/src/auth/repository/in-memory.ts
@@ -1,6 +1,7 @@
-import { left, right } from 'fp-ts/Either';
+import * as E from 'fp-ts/Either';
 import { UserRecord } from '../user-record';
 import { UserRepository } from './type';
+import * as O from 'fp-ts/Option';
 
 const map = new Map<string, UserRecord>();
 
@@ -8,10 +9,18 @@ export const getInMemoryUserRepository = (): UserRepository => {
   return {
     createUserRecord: (record) => {
       if (map.has(record.userId)) {
-        return left(new Error('Found a user with the same id.'));
+        return E.left(new Error('Found a user with the same id.'));
       }
       map.set(record.userId, record);
-      return right(void 0);
+      return E.right(void 0);
+    },
+    getUserRecord: (userId) => {
+      if (map.has(userId)) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        return O.some(map.get(userId)!);
+      }
+
+      return O.none;
     },
   };
 };
--- apps/rest/src/auth/repository/type.ts
+++ apps/rest/src/auth/repository/type.ts
@@ -1,8 +1,13 @@
 import { Either } from 'fp-ts/Either';
 import { UserRecord } from '../user-record';
+import { Option } from 'fp-ts/Option';
 
 export interface CreateUserRecord {
   readonly createUserRecord: (record: UserRecord) => Either<Error, void>;
 }
 
-export type UserRepository = CreateUserRecord;
+export interface GetUserRecord {
+  readonly getUserRecord: (userId: string) => Option<UserRecord>;
+}
+
+export type UserRepository = CreateUserRecord & GetUserRecord;
