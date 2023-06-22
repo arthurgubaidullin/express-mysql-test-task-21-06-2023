1acc77e79b06d42afa45fda8b4f3fac92fedfb34
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-22 13:36:57 +0400

    Add /signin/new_token

--- apps/rest/src/auth/router.ts
+++ apps/rest/src/auth/router.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import { signUpHandler } from './sign-up/handler';
 import { signInHandler } from './sign-in/handler';
 import { infoHandler } from './info/handler';
+import { signInWithRefreshTokenHandler } from './sign-in-with-refresh-token/handler';
 
 const auth = express.Router();
 
 auth.post('/signup', signUpHandler);
 
 auth.post('/signin', signInHandler);
+
+auth.post('/signin/new_token', signInWithRefreshTokenHandler);
 
 auth.get('/info', infoHandler);
 
---
+++ apps/rest/src/auth/sign-in-with-refresh-token/handler.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import * as t from 'io-ts';
+import * as JWT from '../../jwt/jwt';
+import { getUserRepository } from '../repository/instance';
+import { SignInWithRefreshTokenRequest } from './request';
+import { SignInWithRefreshTokenResponse } from './response';
+
+export async function signInWithRefreshTokenHandler(
+  req: express.Request<unknown>,
+  res: express.Response<t.OutputOf<typeof SignInWithRefreshTokenResponse>>
+): Promise<void> {
+  const _body = SignInWithRefreshTokenRequest.decode(req.body);
+
+  if (E.isLeft(_body)) {
+    res.status(400).end();
+    return;
+  }
+  const body = _body.right;
+
+  const _payload = JWT.verify(body.refresh_token);
+  if (O.isNone(_payload)) {
+    res.status(400).json({ ok: false, reason: 'Invalid refresh token.' }).end();
+    return;
+  }
+  const payload = _payload.value;
+
+  const repo = getUserRepository();
+
+  const _record = repo.getUserRecord(payload.userId);
+
+  if (O.isNone(_record)) {
+    res.status(400).json({ ok: false, reason: 'No user found.' }).end();
+    return;
+  }
+  const record = _record.value;
+
+  const accessToken = JWT.sign(record.userId);
+  const refreshToken = JWT.sign(record.userId, 'refresh');
+
+  res
+    .status(200)
+    .json(
+      SignInWithRefreshTokenResponse.encode({
+        ok: true,
+        result: {
+          access_token: accessToken,
+          refresh_token: refreshToken,
+        },
+      })
+    )
+    .end();
+}
