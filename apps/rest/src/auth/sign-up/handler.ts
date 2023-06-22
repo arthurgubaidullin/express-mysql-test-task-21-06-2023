5caefec2f303166b11bea29fc3bd6bd8d04d2fd8
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-22 11:42:44 +0400

    Fix import style

--- apps/rest/src/auth/sign-up/handler.ts
+++ apps/rest/src/auth/sign-up/handler.ts
@@ -1,19 +1,19 @@
 import express from 'express';
 import { getUserRepository } from '../repository/instance';
 import * as UserRecord from '../user-record';
-import { isLeft } from 'fp-ts/Either';
+import * as E from 'fp-ts/Either';
 import { SignUpRequest } from './request';
 import { SignUpResponse } from './response';
-import { OutputOf } from 'io-ts';
-import { sign } from './jwt';
+import * as t from 'io-ts';
+import * as JWT from './jwt';
 
 export async function signUpHandler(
   req: express.Request<unknown>,
-  res: express.Response<OutputOf<typeof SignUpResponse>>
+  res: express.Response<t.OutputOf<typeof SignUpResponse>>
 ): Promise<void> {
   const params = SignUpRequest.decode(req.body);
 
-  if (isLeft(params)) {
+  if (E.isLeft(params)) {
     res.status(400).end();
     return;
   }
@@ -25,11 +25,11 @@ export async function signUpHandler(
     params.right.password
   );
 
-  const accessToken = sign(record.userId);
+  const accessToken = JWT.sign(record.userId);
 
   const result = repo.createUserRecord(record);
 
-  if (isLeft(result)) {
+  if (E.isLeft(result)) {
     res.status(400).json({ ok: false, reason: result.left.message }).end();
     return;
   }
