66328349d43d6a8e1a9cd2160acd7df321050e26
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-22 22:37:43 +0400

    Fix handler

--- apps/rest/src/file/upload-file/handler.ts
+++ apps/rest/src/file/upload-file/handler.ts
@@ -3,10 +3,10 @@ import * as FileRecord from '../file-record';
 import { FileRepository } from '../repository/instance';
 import { OutputOf } from 'io-ts';
 
-export function uploadFileHandler(
+export async function uploadFileHandler(
   req: express.Request<unknown>,
   res: express.Response<OutputOf<typeof FileRecord.FileRecord>>
-): void {
+): Promise<void> {
   if (!req.file) {
     res.status(500).end();
     return;
@@ -14,7 +14,7 @@ export function uploadFileHandler(
 
   const record = FileRecord.create(req.file);
 
-  FileRepository.createFileRecord(record);
+  await FileRepository.createFileRecord(record);
 
   res.status(201).json(FileRecord.FileRecord.encode(record)).end();
 }
