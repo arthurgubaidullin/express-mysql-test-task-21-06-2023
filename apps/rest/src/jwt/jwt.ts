05574776eb7a6b34e577e28bda2fa1bb8578abf4
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-22 13:11:58 +0400

    Add function

--- apps/rest/src/jwt/jwt.ts
+++ apps/rest/src/jwt/jwt.ts
@@ -1,3 +1,4 @@
+import * as O from 'fp-ts/Option';
 import jwt from 'jsonwebtoken';
 import * as Secret from '../secret';
 import { JWTPayload } from './jwt-payload';
@@ -10,4 +11,10 @@ export function sign(userId: string, kind: 'access' | 'refresh' = 'access') {
   return jwt.sign(payload, Secret.get(), {
     expiresIn: kind === 'access' ? '10m' : '1m',
   });
+}
+
+export function verify(token: string): O.Option<JWTPayload> {
+  const payload = jwt.verify(token, Secret.get());
+  const data = JWTPayload.decode(payload);
+  return O.fromEither(data);
 }
