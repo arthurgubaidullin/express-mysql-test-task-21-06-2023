f28287324448741b7c323e2fcf69a418fa70e775
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-22 13:25:57 +0400

    Add test

---
+++ apps/rest-e2e/src/rest/auth/sign-in-with-refresh-token.spec.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import * as Email from './email';
+
+describe('POST /signin/new_token', () => {
+  let id: string | null = null;
+  const password = 'abcdef12345';
+  let refreshToken: string | null = null;
+
+  beforeEach(async () => {
+    id = Email.generate();
+    refreshToken = (
+      await axios.post(`/signup`, {
+        id,
+        password,
+      })
+    ).data.result.refresh_token;
+  });
+
+  it('should sign in', async () => {
+    const res = await axios.post(
+      `/signin/new_token`,
+      {
+        token: refreshToken,
+      },
+      { validateStatus: () => true }
+    );
+
+    expect(res.status).toBe(201);
+    expect(res.data.result.access_token).toBeTruthy();
+    expect(res.data.result.refresh_token).toBeTruthy();
+  });
+});
