ba13cf21f65635ad2a0b8be54345a3b0c3efc63a
Artur Gubaidullin <lapteuh@gmail.com>
2023-06-22 11:41:10 +0400

    Fix repo

--- apps/rest/src/auth/repository/in-memory.ts
+++ apps/rest/src/auth/repository/in-memory.ts
@@ -1,3 +1,4 @@
+import { left, right } from 'fp-ts/Either';
 import { UserRecord } from '../user-record';
 import { UserRepository } from './type';
 
@@ -6,9 +7,11 @@ const map = new Map<string, UserRecord>();
 export const getInMemoryUserRepository = (): UserRepository => {
   return {
     createUserRecord: (record) => {
-      if (!map.has(record.userId)) {
-        map.set(record.userId, record);
+      if (map.has(record.userId)) {
+        return left(new Error('Found a user with the same id.'));
       }
+      map.set(record.userId, record);
+      return right(void 0);
     },
   };
 };
--- apps/rest/src/auth/repository/type.ts
+++ apps/rest/src/auth/repository/type.ts
@@ -1,7 +1,8 @@
+import { Either } from 'fp-ts/Either';
 import { UserRecord } from '../user-record';
 
 export interface CreateUserRecord {
-  readonly createUserRecord: (record: UserRecord) => void;
+  readonly createUserRecord: (record: UserRecord) => Either<Error, void>;
 }
 
 export type UserRepository = CreateUserRecord;
